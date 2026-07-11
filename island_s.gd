77e949e2073eec13f07c7fa29824f1a59c446f5a
Ari Alexander Tannahill Thomson <ari_alexander@outlook.com>
2026-07-11 12:41:34 +0100

    Setup

--- island_s.gd
+++ island_s.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@export var speed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_forward"):
-		print("Woah!")
-		
