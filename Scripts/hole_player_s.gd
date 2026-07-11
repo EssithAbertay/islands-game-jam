e1590252a3418073699f01b2570212b1bc604488
toddgilc <toddgilc@gmail.com>
2026-07-11 17:53:50 +0100

    eating based on size and nicer scaling

--- Scripts/hole_player_s.gd
+++ Scripts/hole_player_s.gd
@@ -3,14 +3,44 @@ extends CharacterBody3D
 # How fast the player moves in meters per second.
 @export var speed = 14
 @export var player_score: int = 0
-@export var scale_rate: int = 1.05
+@export var scale_rate: float = 1.001
+@export var scale_up_timer_max: float = .8
+var scale_up_timer: float = 0
+var new_scale: Vector3 = Vector3(0,0,0)
+var scaling: bool = false
 
+#need x xp to beat a stage which will up ur size and reset ur xp 
+@export var current_stage: int = 0
+@export var stage_count: int = 20
+@export var xp: int = 0
+@export var xp_count: int = 10
 
-var target_velocity = Vector3.ZERO
-
-func _physics_process(delta):
+func _process(delta):
+	# scaling
+	scale_up_timer += delta
+	if (scaling == true):
+		if (scale_up_timer <= scale_up_timer_max):
+			new_scale = scale * scale_rate
+			scale = new_scale + (scale - new_scale) * scale_up_timer
+		else: 
+			scale_up_timer = 0
+			scaling = false
 	
-	var direction = Vector3.ZERO
+	
+
+func _on_collection_area_area_entered(area: Area3D) -> void:
+	if area.is_in_group("pickup"):
+		if "point_value" in area && (scale >= area.scale):
+		# 1. Access the pickup data and add points
+			player_score += area.point_value
+			print("Player Score: ", player_score)
+			scaling = true
+			scale_up_timer = 0
+			area.queue_free()
+		
+	pass # Replace with function body.
+	
+func _physics_process(delta):
 
 	if Input.is_action_pressed("move_right"):
 		position.x += 1
@@ -20,18 +50,4 @@ func _physics_process(delta):
 		position.z += 1
 	if Input.is_action_pressed("move_forward"):
 		position.z -= 1
-
-
 	move_and_slide()
-
-
-func _on_collection_area_area_entered(area: Area3D) -> void:
-	if area.is_in_group("pickup"):
-		if "point_value" in area:
-		# 1. Access the pickup data and add points
-			player_score += area.point_value
-			print("Player Score: ", player_score)
-			scale *= scale_rate
-			area.queue_free()
-		
-	pass # Replace with function body.
--- Scripts/pickup_manager_s.gd
+++ Scripts/pickup_manager_s.gd
@@ -4,10 +4,11 @@ var pickup = preload("res://Scenes/points_pickup.tscn")
 var timerMax: float = 3
 var timer: float = 0
 var spawnLocation: Vector3 = Vector3(0,0,0)
+var spawnSize: float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	randomize()
 	pass # Replace with function body.
 
 
@@ -24,6 +25,8 @@ func _process(delta: float) -> void:
 
 func spawnPickup():
 	var new_pickup = pickup.instantiate()
+	spawnLocation = Vector3(randf_range(-10.0, 10.0), 0, randf_range(-10.0, 10.0))
+	spawnSize = randf_range(1, 10.0)
 	new_pickup.global_position = spawnLocation
-
+	new_pickup.scale = Vector3(spawnSize, spawnSize, spawnSize)
 	add_child(new_pickup)
