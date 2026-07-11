f81537852bd18071d95d95ed712e34a793012640
toddgilc <toddgilc@gmail.com>
2026-07-11 13:05:54 +0100

    Moving player

---
+++ Scenes/hole_player_s.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		position.x += 1
+	if Input.is_action_pressed("move_left"):
+		position.x -= 1
+	if Input.is_action_pressed("move_back"):
+		position.z += 1
+	if Input.is_action_pressed("move_forward"):
+		position.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
