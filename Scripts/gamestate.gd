4578d873a11dd9b2fc1f50107adc53c392457e0c
Ari Alexander Tannahill Thomson <ari_alexander@outlook.com>
2026-07-12 17:14:17 +0100

    more changes

--- Scripts/gamestate.gd
+++ Scripts/gamestate.gd
@@ -1,10 +1,14 @@
 extends Node
 
 var player_score: int = 0
-enum Mode {ATTACK, DEFEND, SETUP}
+enum Mode {ATTACK, DEFEND, SETUP, SETUPATTACK}
 var mode: Mode = Mode.ATTACK
 var swapping_mode: bool = false
 var current_stage: int = 0
+var turretCost:int = 10
+var turretCostScaling = 1.1
+var spawnWave: bool = false
+var numberOfWavesToSpawn: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Scripts/island_s.gd
+++ Scripts/island_s.gd
@@ -1,8 +1,5 @@
 extends Node
 
-@export var enemy_spawn_rate:float = 1
-var enemy_spawn_cd = 0
-
 @export var boat: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	# only spawn enemies when in defence mode
-	if(GameState.mode == GameState.Mode.DEFEND):
-		enemy_spawn_cd += delta
-		if(enemy_spawn_cd >= enemy_spawn_rate):
-			enemy_spawn_cd = 0
-		
+	if(GameState.spawnWave):
 			var boat_instance = boat.instantiate()
 			add_child(boat_instance)
 			boat_instance.global_position.y = -1
